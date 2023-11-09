8687e1c081ac788bb9b7471e50d843a880e7229f
Missael Padilla <lmpadillar@gmail.com>
2023-11-09 13:49:50 -0800

    Set config

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent {
+  title: string = '';
 
+  setTitle(title: string) {
+    this.title = title;
+  }
 }
