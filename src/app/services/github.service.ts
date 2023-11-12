54773d417484ccdc177d74eb1c82e1516f8814ff
Missael Padilla <lmpadillar@gmail.com>
2023-11-11 18:37:39 -0800

    filter only front projects

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -20,7 +20,8 @@ export class GithubService {
     });
     return this.http.get<Netlify[]>(`${this.apiUrl}`, { headers })
     .pipe( map((resp: Netlify[]) => {
-      const users = resp.map(
+      console.log(resp);
+      const users = resp.filter(f => f.use_functions === null).map(
         (site) =>
           new NetliFySiteClass(
             site.name.replaceAll('-',' ').replace('missael padilla',''),
